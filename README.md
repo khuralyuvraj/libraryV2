da840946e564195c18cb26abda3b75a6b9176564
yotsuha <khuralyuvraj+yotsuha@gmail.com>
2024-12-16 10:14:57 -0600

    add mediaobject properties

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tasks
+- [ ] add new item to the system
+- [ ] check item out/in by title
+- [ ] determine how many copies of an item are available, by title
+- [ ] display all books in the system
+- [ ] display all movies in the system
+- [ ] display all games in the system
+- [ ] display all items in the system
+- [ ] check whether an item exists in the system (by title), and show details if it exists
+- [ ] view all items in the system published in a certain year
+- [ ] csv import/export
+
+## tracked info about each item
+- [x] title
+- [ ] year published
+- [ ] number of copies in the system
+- [ ] how many copies are checked out and how many are available
+- [ ] the names of the people who have checked it out (if any are checked out)
--- src/MediaObject.java
+++ src/MediaObject.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public abstract class MediaObject {
     Scanner scn =  new Scanner(System.in);
     private String title;
+    private int yearPublished;
+    private int copies;
     private boolean isCheckedIn = true;
     private String checkOutPerson;
 
@@ -16,15 +18,26 @@ public abstract class MediaObject {
     public void setTitle(String title){
         this.title = title;
     }
+    public int getYearPublished(){
+        return yearPublished;
+    }
+    public void setYearPublished(int yearPublished){
+        this.yearPublished = yearPublished;
+    }
+    public int getCopies(){
+        return copies;
+    }
+    public void addCopies(){
+        this.copies++;
+    }
     public boolean getCheckedStatus(){
         return isCheckedIn;
     }
     public String getCheckedOutPerson(){
         return checkOutPerson;
     }
-    public void checkOut(){
-        System.out.print("Who's checking out the book: ");
-        this.checkOutPerson = scn.nextLine();
+    public void checkOutByName(String checkOutPerson){
+        this.checkOutPerson = checkOutPerson;
         this.isCheckedIn = false;
     }
     public void checkIn(){
